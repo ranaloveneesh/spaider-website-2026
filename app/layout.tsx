d8452d702545f487d5f701fb15bb0ff8a0e776d0
Md Sohail Ansari <sohail.ansari@spaiderspace.com>
2026-07-21 12:35:57 +0530

    test deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ export default function RootLayout({
 			<body className="min-h-full flex flex-col bg-spx-void text-foreground">
 				<JsonLd data={getSiteJsonLd()} />
 				<Toaster />
+
 				<div className="relative z-10">{children}</div>
 			</body>
 		</html>
